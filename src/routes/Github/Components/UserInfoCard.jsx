b93d8f0ab9fe78b43a86c30328ffbb5d6e6abe65
ecmadao <wlec@outlook.com>
2016-11-28 21:24:08 +0800

    UI change

--- src/routes/Github/Components/UserInfoCard.jsx
+++ src/routes/Github/Components/UserInfoCard.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 
+const CARD_COLORS = [
+  'rgba(52, 143, 223, 1)',
+  'rgba(52, 143, 223, 0.8)',
+  'rgba(52, 143, 223, 0.6)',
+  'rgba(52, 143, 223, 0.4)',
+  'rgba(52, 143, 223, 0.2)'
+];
+
+const getUserCardColor = (user) => {
+  const {public_repos, followers, following} = user;
+  const obj = {
+    public_repos,
+    followers,
+    following
+  };
+  Object.keys(obj).sort((thisKey, nextKey) => {
+    return parseInt(obj[nextKey]) - parseInt(obj[thisKey]);
+  }).forEach((key, index) => {
+    obj[key] = CARD_COLORS[index];
+  });
+  return obj;
+};
+
 const UserInfoCard = (props) => {
   const {user} = props;
   if (!user) { return (<div></div>) }
+  const userCardColor = getUserCardColor(user);
   return (
     <div className="info_card_container">
       <p>基本信息</p>
@@ -21,6 +45,7 @@ const UserInfoCard = (props) => {
           <a
             target="_blank"
             className="info_social"
+            style={{backgroundColor: userCardColor['public_repos']}}
             href={`https://github.com/${user.login}?tab=repositories`}>
             <span>{user['public_repos']}</span>&nbsp;
             <span>repositories</span>
@@ -28,6 +53,7 @@ const UserInfoCard = (props) => {
           <a
             target="_blank"
             className="info_social"
+            style={{backgroundColor: userCardColor['followers']}}
             href={`https://github.com/${user.login}?tab=followers`}>
             <span>{user.followers}</span>&nbsp;
             <span>followers</span>
@@ -35,6 +61,7 @@ const UserInfoCard = (props) => {
           <a
             target="_blank"
             className="info_social"
+            style={{backgroundColor: userCardColor['following']}}
             href={`https://github.com/${user.login}?tab=following`}>
             <span>{user.following}</span>&nbsp;
             <span>following</span>
