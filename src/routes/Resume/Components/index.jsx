9826c07891effba6ad873c505709c6b30c5d2585
ecmadao <wlec@outlook.com>
2016-12-03 11:17:41 +0800

    initial nav

--- src/routes/Resume/Components/index.jsx
+++ src/routes/Resume/Components/index.jsx
@@ -1,17 +1,57 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import classNames from 'classnames';
 import { bindActionCreators } from 'redux';
 
 import '../styles/resume.css';
+import { RESUME_SECTIONS } from '../helper/const_value';
 
 class Resume extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeSection: RESUME_SECTIONS[0].id
+    };
+    this.handleSectionChange = this.handleSectionChange.bind(this);
+  }
+
+  handleSectionChange(id) {
+    const {activeSection} = this.state;
+    if (activeSection !== id) {
+      this.setState({
+        activeSection: id
+      });
+    }
+  }
+
+  renderNavigation() {
+    const {activeSection} = this.state;
+    return RESUME_SECTIONS.map((section, index) => {
+      const {id, text} = section;
+      const sectionClass = classNames('resume_section', {
+        'active': activeSection === id
+      });
+      return (
+        <div className={sectionClass} key={index}>
+          <div
+            className="section_wrapper"
+            onClick={() => this.handleSectionChange(id)}>
+            {text}
+          </div>
+        </div>
+      )
+    });
+  }
+
   render() {
     return (
       <div className="resume_container">
-        
+        <div className="resume_navigation">
+          {this.renderNavigation()}
+        </div>
       </div>
     )
   }
 }
 
-export default Resume;
+export default connect()(Resume);
---
+++ src/routes/Resume/helper/const_value.js
@@ -0,0 +1,22 @@
+export const RESUME_SECTIONS = [
+  {
+    id: 'info',
+    text: '基本信息'
+  },
+  {
+    id: 'educations',
+    text: '教育经历'
+  },
+  {
+    id: 'workExperiences',
+    text: '工作经历'
+  },
+  {
+    id: 'personalProjects',
+    text: '个人项目'
+  },
+  {
+    id: 'others',
+    text: '其他补充'
+  }
+];
