c2251344723462cbf9557914a9527713e893e7ba
ecmadao <wlec@outlook.com>
2016-12-07 08:32:58 +0800

    label component

---
+++ src/components/Labels/index.jsx
@@ -0,0 +1,89 @@
+import React, { PropTypes } from 'react';
+
+import Label from './Label';
+import './labels.css';
+import Input from 'COMPONENTS/Input';
+
+class Labels extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+    this.onDelete = this.onDelete.bind(this);
+    this.onKeyDown = this.onKeyDown.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  onDelete(index) {
+    const { onDelete } = this.props;
+    return () => {
+      onDelete && onDelete(index);
+    }
+  }
+
+  onKeyDown(e) {
+    const { value } = this.state;
+    if (e.keyCode === 13 && value) {
+      const { onAdd } = this.props;
+      onAdd && onAdd(value);
+      this.clearInput();
+    }
+  }
+
+  clearInput() {
+    this.setState({ value: '' });
+  }
+
+  handleInputChange(value) {
+    value && this.setState({ value })
+  }
+
+  renderLabels() {
+    const { labels, color } = this.props;
+    return labels.map((label, index) => {
+      return (
+        <Label
+          key={index}
+          value={label}
+          color={color}
+          onDelete={this.onDelete(index)}
+        />
+      )
+    })
+  }
+
+  render() {
+    const { value } = this.state;
+    return (
+      <div className="labels_wrapper">
+        {this.renderLabels()}
+        <div className="label_input_wrapper">
+          <Input
+            value={value}
+            style="borderless underline"
+            placeholder="添加使用的技术"
+            onChange={this.handleInputChange}
+            onKeyDown={this.onKeyDown}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+Labels.propTypes = {
+  color: PropTypes.string,
+  labels: PropTypes.array,
+  onDelete: PropTypes.func,
+  onAdd: PropTypes.func
+};
+
+Labels.defaultProps = {
+  labels: [],
+  color: 'grey',
+  onDelete: () => {},
+  onAdd: () => {}
+};
+
+export default Labels;
