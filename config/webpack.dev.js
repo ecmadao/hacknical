1eec34f750aa69a481cd36d57c5cfa308043ea5a
ecmadao <wlec@outlook.com>
2016-12-11 00:24:08 +0800

    use webpack-dashboard the correct the way

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,11 +1,9 @@
 const webpack = require('webpack');
-const Dashboard = require('webpack-dashboard');
 const DashboardPlugin = require('webpack-dashboard/plugin');
-const dashboard = new Dashboard();
 
 const config = require('./webpack.config');
 
-config.plugins.push(new DashboardPlugin(dashboard.setData));
+config.plugins.push(new DashboardPlugin());
 config.debug = true;
 config.devtool = "cheap-module-eval-source-map";
 
