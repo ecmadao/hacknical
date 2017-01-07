768633abb71c8bcd01906407c0842914ef636a0c
ecmadao <wlec@outlook.com>
2017-01-07 22:33:32 +0800

    bugfixed in mongoose connect

--- app/models/mongoose/index.js
+++ app/models/mongoose/index.js
@@ -3,12 +3,19 @@ import config from 'config';
 
 const mongodbUrl = config.get('database.url');
 
-mongoose.connect(mongodbUrl, function (err) {
+function handleErr(err) {
   if (err) {
     console.error('connect to %s error: ', mongodbUrl, err.message);
     process.exit(1);
   }
-});
+}
+
+if (process.env.NODE_ENV === 'production') {
+  mongoose.connect(mongodbUrl, { auth: { authdb: "admin" } }, handleErr);
+} else {
+  mongoose.connect(mongodbUrl, handleErr);
+}
+
 mongoose.Promise = global.Promise;
 
 export default mongoose;
