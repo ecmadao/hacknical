02768a8475e5838d5a0a94a58200449aecada232
ecmadao <wlec@outlook.com>
2017-01-26 02:32:48 +0800

    remove console

--- app/middlewares/cdn.js
+++ app/middlewares/cdn.js
@@ -3,10 +3,7 @@ const config = require('config');
 const APP_NAME = config.get('appName');
 const CDN = config.get('cdn');
 const CDN_URL = `${CDN}/${APP_NAME}`;
-console.log(process.env)
-console.log(process.env.npm_package_version)
 const APP_VERSION = process.env.APP_VERSION;
-console.log(`APP_VERSION: ${APP_VERSION}`)
 
 const URL = APP_VERSION ? `${CDN_URL}/${APP_VERSION}` : CDN_URL;
 
