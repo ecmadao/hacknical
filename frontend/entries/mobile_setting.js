b814e7c88df9f747ee8e87c65274a8db3d3ea8f3
ecmadao <wlec@outlook.com>
2017-03-26 14:48:39 +0800

    topbar font family bugfixed

--- frontend/entries/mobile_setting.js
+++ frontend/entries/mobile_setting.js
@@ -1,3 +1,4 @@
+import 'STYLES/fonts-login.css';
 import renderApp from 'PAGES/mobile/setting';
 
 $(() => {
--- frontend/entries/mobile_share.js
+++ frontend/entries/mobile_share.js
@@ -1,3 +1,4 @@
+import 'STYLES/fonts-login.css';
 import renderApp from 'PAGES/mobile/share';
 import 'SRC/vendor/share/share_mobile.css';
 
