d759955411461451299d410563b98db4ef96e9bd
ecmadao <wlec@outlook.com>
2017-03-26 08:46:33 +0800

    initial ip location

--- app/controllers/helper/platform.js
+++ app/controllers/helper/platform.js
@@ -1,9 +1,9 @@
-// import geoip from 'geoip-lite';
+import geoip from 'geoip-lite';
 
 const checkPlatform = async (ctx, next) => {
   const userAgent = ctx.state.userAgent;
-  // const geo = geoip.lookup(ctx.request.ip);
-  // console.log(geo);
+  const geo = geoip.lookup(ctx.request.ip);
+  console.log(geo);
   ctx.state.browser = userAgent.browser;
   ctx.state.platform = userAgent.platform;
   ctx.state.isMobile = userAgent.isMobile;
