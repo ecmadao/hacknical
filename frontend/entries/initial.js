387e8f832a25160538d0d06469e28b560803bf6d
ecmadao <wlec@outlook.com>
2017-03-31 22:39:04 +0800

    extract emoji

--- frontend/entries/initial.js
+++ frontend/entries/initial.js
@@ -1,6 +1,14 @@
 import Api from 'API';
 import Rock from 'SRC/vendor/initial';
 
+const EMOJI = {
+  rocket: '🚀',
+  winking: '😉',
+  heartEyes: '😍',
+  smiling: '😝',
+  heart: '\u2764\uFE0F'
+};
+
 $(() => {
   $(document).bind("contextmenu", (e) => {
     return false;
@@ -11,17 +19,17 @@ $(() => {
   rock
     .roll('start fetching your informations')
     .then(instance => instance.loading())
-    .then(instance => instance.roll('🚀 fetching repositories'))
+    .then(instance => instance.roll(`${EMOJI.rocket} fetching repositories`))
     .then(instance => instance.loading())
     .then(() => Api.github.fetchRepos())
-    .then(result => rock.roll(`${result} 😎`))
-    .then(instance => instance.roll('🚀 fetching commits info'))
+    .then(result => rock.roll(`${result} ${EMOJI.winking}`))
+    .then(instance => instance.roll(`${EMOJI.rocket} fetching commits info`))
     .then(instance => instance.loading())
     .then(() => Api.github.fetchCommits())
-    .then(result => rock.roll(`${result} 😉`))
-    .then(instance => instance.roll('🚀 fetching your organizations info'))
+    .then(result => rock.roll(`${result} ${EMOJI.heartEyes}`))
+    .then(instance => instance.roll(`${EMOJI.rocket} fetching your organizations info`))
     .then(instance => instance.loading())
     .then(() => Api.github.fetchOrgs())
-    .then(result => rock.roll(`${result} 😝`))
+    .then(result => rock.roll(`${result} ${EMOJI.smiling}`))
     .then(instance => instance.stop())
 });
