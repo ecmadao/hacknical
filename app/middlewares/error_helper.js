fbe71605b1ace33bb5859467b7ad6d5838d8513c
ecmadao <wlec@outlook.com>
2018-02-06 17:05:54 +0800

    WHAT: Handle old dashboard url request
    WHY:
    
    * XXXXX
    
    HOW:
    
    * NOTHING TO SAY

--- app/middlewares/error_helper.js
+++ app/middlewares/error_helper.js
@@ -4,6 +4,13 @@ const catchError = () => async (ctx, next) => {
   try {
     await next();
     const { url } = ctx;
+
+    if (/^\/dashboard/g.test(url)) {
+      logger.info(`[OLD URL REQUEST][${ctx.status}][${url}]`);
+      const { githubLogin } = ctx.session;
+      return await ctx.redirect(`/${githubLogin}`);
+    }
+
     if (ctx.status === 404) {
       const login = url.split('/')[0];
       await ctx.redirect(`/${login}`);
