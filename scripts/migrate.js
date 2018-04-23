fa9913d0257c9f3b2b865f6fc08db21cf0449a6f
ecmadao <wlec@outlook.com>
2018-04-23 22:43:53 +0800

    WHAT: clear
    WHY:
    
    * XXXXX
    
    HOW:
    
    * NOTHING TO SAY

--- scripts/migrate.js
+++
@@ -1,7 +0,0 @@
-require('babel-core/register')();
-require('babel-polyfill');
-
-// TODO:
-// migrate share-analysis, use type instead of url
-// require('./migrate/resumeHash');
-require('./migrate/share');
