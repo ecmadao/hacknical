ffea9839d2ca7eb27055807f4b8abc4900db0957
ecmadao <wlec@outlook.com>
2018-12-14 00:07:36 +0800

    WHAT: Change resume download
    WHY:
    
    * XXXXX
    
    HOW:
    
    * NOTHING TO SAY

--- app/services/downloads.js
+++ app/services/downloads.js
@@ -1,72 +1,75 @@
 
-import phantom from 'phantom';
-import path from 'path';
-import fs from 'fs-extra';
-import klawSync from 'klaw-sync';
-import config from 'config';
-import logger from '../utils/logger';
+import phantom from 'phantom'
+import path from 'path'
+import fs from 'fs-extra'
+import klawSync from 'klaw-sync'
+import config from 'config'
+import logger from '../utils/logger'
+// import webpage from 'webpage'
 
-const sourcePath = config.get('downloads');
+const sourcePath = config.get('downloads')
 
 const waitUntil = asyncFunc => new Promise((resolve, reject) => {
   const wait = () => {
     asyncFunc().then((value) => {
       if (value === true) {
-        resolve();
+        resolve()
       } else {
-        setTimeout(wait, 100);
+        setTimeout(wait, 100)
       }
-    }).catch((e) => {
-      reject(e);
-    });
+    }).catch(reject)
   }
-  wait();
-});
+  wait()
+})
 
-const clearFolder = folder =>
-  process.nextTick(() => {
-    const files = klawSync(folder, { nodir: true });
-    for (const file of files) {
-      fs.removeSync(file.path);
-    }
-  });
+const clearFolder = (folder) => {
+  const files = klawSync(folder, { nodir: true })
+  for (const file of files) {
+    fs.removeSync(file.path)
+  }
+}
 
 const renderScreenshot = async (input, output) => {
-  const instance = await phantom.create();
-  const page = await instance.createPage();
+  const instance = await phantom.create()
+  const page = await instance.createPage()
 
-  await page.property('viewportSize', { width: 1024, height: 600 });
-  await page.open(input);
+  await page.property('paperSize', {
+    width: 1024,
+    height: 1448,
+    format: 'A4',
+    orientation: 'portrait'
+  })
+  await page.open(input)
 
-  await waitUntil(() => page.evaluate(() => window.done));
-  await page.render(output);
-  await instance.exit();
-};
+  await waitUntil(() => page.evaluate(() => window.done))
+  await page.render(output)
+  await instance.exit()
+}
 
 const downloadResume = async (url, options = {}) => {
   const {
     title,
     folder
-  } = options;
-  const resultFloder = path.resolve(__dirname, sourcePath, folder);
+  } = options
+  const resultFloder = path.resolve(__dirname, sourcePath, folder)
 
   // makesure folder exist
-  await fs.ensureDirSync(sourcePath);
-  await fs.ensureDir(resultFloder);
+  await fs.ensureDirSync(sourcePath)
+  await fs.ensureDir(resultFloder)
 
-  const filePath = path.resolve(resultFloder, title);
-  const resultPath = `/downloads/${folder}/${title}`;
-  logger.info(`[RESUME:DOWNLOAD:RENDER-PATH] ${filePath}`);
+  const filePath = path.resolve(resultFloder, title)
+  const resultPath = `/downloads/${folder}/${title}`
+  logger.info(`[RESUME:DOWNLOAD:RENDER-PATH] ${filePath}`)
 
   if (fs.existsSync(filePath)) {
-    return resultPath;
+    return resultPath
   }
 
-  clearFolder(resultFloder);
-  await renderScreenshot(url, filePath);
-  return resultPath;
-};
+  clearFolder(resultFloder)
+  await renderScreenshot(url, filePath)
+  return resultPath
+}
 
 export default {
   resume: downloadResume
-};
+}
