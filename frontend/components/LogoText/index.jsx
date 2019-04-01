d5a0e27bcec91764d23a87c8ce86f65551e3f1ed
ecmadao <wlec@outlook.com>
2019-04-01 22:28:32 +0800

    add missing animation

--- frontend/components/LogoText/index.jsx
+++ frontend/components/LogoText/index.jsx
@@ -2,9 +2,12 @@
 import React from 'react'
 import cx from 'classnames'
 import styles from './logo.css'
+import { AnimationComponent } from 'light-ui'
 
-const LogoText = (props) => {
+const _LogoText = (props) => {
   const {
+    status,
+    onTransitionEnd,
     className,
     theme = 'light',
     onClick = Function.prototype
@@ -15,14 +18,22 @@ const LogoText = (props) => {
       className={cx(
         styles.logoContainer,
         className,
-        styles[`logo_${theme}`]
+        styles[`logo_${theme}`],
+        styles[`logoContainer-${status}`]
       )}
       onClick={onClick}
+      onTransitionEnd={onTransitionEnd}
     >
       <div className={styles.logoFront}>hacknical</div>
       <div className={styles.logoBack}>hacknical</div>
     </div>
   )
 }
+
+const LogoText = props => (
+  <AnimationComponent>
+    <_LogoText {...props} />
+  </AnimationComponent>
+)
 
 export default LogoText
