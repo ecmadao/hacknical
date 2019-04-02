acb9f99ce31d14eedb1476bb84205d8bffc2db8f
ecmadao <wlec@outlook.com>
2019-04-02 12:40:14 +0800

    add missing onFinish callback

--- frontend/entries/404.js
+++ frontend/entries/404.js
@@ -9,8 +9,8 @@ $(() => {
     wordLines: [
       '$ seems nothing could found',
       '$ start redirecting....',
-      '$ 5 4 3 2 1 0'
+      '$ 5   4   3   2   1   0'
     ],
-    // onFinish: () => window.location = '/'
+    onFinish: () => window.location = '/'
   })
 })
