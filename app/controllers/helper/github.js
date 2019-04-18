36f08eb116f17376284086f51c6649bfb9fa5f28
ecmadao <wlec@outlook.com>
2019-04-18 08:52:32 +0800

    avoid duplicate commit data

--- app/controllers/helper/github.js
+++ app/controllers/helper/github.js
@@ -1,8 +1,9 @@
-import dateHelper from '../../utils/date';
-import logger from '../../utils/logger';
 
-const { getMonth, getDateBySeconds } = dateHelper;
-const BASE_DAYS = [0, 0, 0, 0, 0, 0, 0];
+import dateHelper from '../../utils/date'
+import logger from '../../utils/logger'
+
+const { getMonth, getDateBySeconds } = dateHelper
+const BASE_DAYS = [0, 0, 0, 0, 0, 0, 0]
 
 export const combineReposCommits = (reposCommits) => {
   const result = {
@@ -20,15 +21,15 @@ export const combineReposCommits = (reposCommits) => {
       // 1 to 12 month, each month commits count & new repos count
       // 1: { repos: [xxx, yyy], commitsCount: xxx }
     }
-  };
-
-  if (!reposCommits.length) {
-    logger.error('[COMMITS:EMPTY]');
-    return result;
   }
 
-  const commitsTmp = {};
-  const dailyCommitsTmp = {};
+  if (!reposCommits.length) {
+    logger.error('[COMMITS:EMPTY]')
+    return result
+  }
+
+  const commitsTmp = {}
+  const dailyCommitsTmp = {}
 
   // initial monthReview
   for (let i = 1; i < 13; i += 1) {
@@ -40,62 +41,66 @@ export const combineReposCommits = (reposCommits) => {
     result.monthReview[i] = {
       repos: [],
       commitsCount: 0
-    };
+    }
   }
 
+  const tmp = new Set()
+
   for (const repository of reposCommits) {
-    const { created_at, commits, name } = repository;
-    const month = getMonth(created_at);
-    result.monthReview[month].repos.push(name);
+    const { created_at, commits, name } = repository
+    if (tmp.has(name)) continue
+    tmp.add(name)
+
+    const month = getMonth(created_at)
+    result.monthReview[month].repos.push(name)
 
     for (const commit of commits) {
-      const { total, days, week } = commit;
-      result.total += total;
+      const { total, days, week } = commit
+      result.total += total
 
       if (commitsTmp[week] === undefined) {
         commitsTmp[week] = {
           week,
           total: 0,
-          days: [...BASE_DAYS],
+          days: [...BASE_DAYS]
         }
       }
-      commitsTmp[week].total += total;
+      commitsTmp[week].total += total
 
       for (let index = 0; index < days.length; index += 1) {
-        const dayCommit = days[index];
-        commitsTmp[week].days[index] += dayCommit;
+        const dayCommit = days[index]
+        commitsTmp[week].days[index] += dayCommit
 
         if (dayCommit) {
-          dailyCommitsTmp[index].total += dayCommit;
-          const average = dailyCommitsTmp[index].avg;
-          const len = dailyCommitsTmp[index].commits.length;
-          dailyCommitsTmp[index].avg = ((average * len) + dayCommit) / (len + 1);
+          dailyCommitsTmp[index].total += dayCommit
+          const average = dailyCommitsTmp[index].avg
+          const len = dailyCommitsTmp[index].commits.length
+          dailyCommitsTmp[index].avg = ((average * len) + dayCommit) / (len + 1)
           dailyCommitsTmp[index].commits.push(dayCommit)
         }
 
-        const daySeconds = week - ((7 - index) * 24 * 60 * 60);
-        const targetMonth = getDateBySeconds(daySeconds, 'M');
-        result.monthReview[targetMonth].commitsCount += dayCommit;
+        const daySeconds = week - ((7 - index) * 24 * 60 * 60)
+        const targetMonth = getDateBySeconds(daySeconds, 'M')
+        result.monthReview[targetMonth].commitsCount += dayCommit
       }
     }
   }
 
   for (const weekday of Object.keys(dailyCommitsTmp)) {
-    result.dailyCommits.push(dailyCommitsTmp[weekday].avg);
-    result.totalDailyCommits.push(dailyCommitsTmp[weekday].total);
+    result.dailyCommits.push(dailyCommitsTmp[weekday].avg)
+    result.totalDailyCommits.push(dailyCommitsTmp[weekday].total)
   }
 
-  result.commits = Object.keys(commitsTmp).map(week => commitsTmp[week]);
-  return result;
-};
+  result.commits = Object.keys(commitsTmp).map(week => commitsTmp[week])
+  return result
+}
 
 export const getReposLanguages = (repos) => {
-  const languages = new Set();
+  const languages = new Set()
 
   for (const repository of repos) {
-    const { language } = repository;
-    language && languages.add(language);
+    const { language } = repository
+    language && languages.add(language)
   }
-  return [...languages.values()];
-};
-
+  return [...languages.values()]
+}
