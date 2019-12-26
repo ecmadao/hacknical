f21841aa1ce7b0545c8756efcff475ef6f5f8a3f
ecmadao <wlec@outlook.com>
2019-12-26 20:46:34 +0800

    update log formatter

--- app/controllers/github.js
+++ app/controllers/github.js
@@ -190,7 +190,30 @@ const getShareLogs = async (ctx) => {
 
   ctx.body = {
     success: true,
-    result: logs
+    result: Object.values(
+      logs.reduce((dict, log) => {
+        const {
+          _id,
+          login,
+          type,
+          updatedAt,
+          createdAt,
+          ...others
+        } = log
+        if (!dict[others.ipInfo.ip]) {
+          dict[others.ipInfo.ip] = {
+            ip: others.ipInfo.ip,
+            addr: others.ipInfo.addr,
+            datetime: others.datetime,
+            browser: others.browser,
+            platform: others.platform,
+            data: []
+          }
+        }
+        dict[others.ipInfo.ip].data.push(others)
+        return dict
+      }, {})
+    ).sort((log1, log2) => new Date(log2.datetime).getTime() - new Date(log1.datetime).getTime())
   }
 }
 
--- frontend/pages/dashboard/routes/Records/Components/Desktop/container/LogCard.jsx
+++ frontend/pages/dashboard/routes/Records/Components/Desktop/container/LogCard.jsx
@@ -2,6 +2,8 @@
 import React from 'react'
 import cx from 'classnames'
 import {
+  Tipso,
+  Label,
   Loading,
   ClassicCard,
 } from 'light-ui'
@@ -11,6 +13,17 @@ import dateHelper from 'UTILS/date'
 
 const { minutesBefore } = dateHelper.relative
 const recordsTexts = locales('dashboard').records.common
+
+const LogTipso = props => (
+  <div className={styles.logTipso}>
+    <div className={styles.logTipsoMain}>
+      {props.log.platform}, {props.log.browser}
+    </div>
+    <div className={styles.logTipsoSub}>
+      {props.log.datetime}
+    </div>
+  </div>
+)
 
 export const LogCard = (props) => {
   const { loading, viewLogs } = props
@@ -34,15 +47,36 @@ export const LogCard = (props) => {
         {viewLogs.map((viewLog, index) => (
           <div key={index} className={styles.viewLog}>
             <div className={styles.viewLogTop}>
-              {viewLog.ipInfo.ip}
-              &nbsp;&nbsp;
-              {viewLog.platform}, {viewLog.browser}
+              <div className={styles.viewRow}>
+                {viewLog.ip}
+                &nbsp;&nbsp;
+                {viewLog.platform}, {viewLog.browser}
+              </div>
+              <Tipso
+                theme="dark"
+                trigger="click"
+                tipsoContent={(
+                  <div className={styles.logTipsoContainer}>
+                    {viewLog.data.map((logData, index) => (
+                      <LogTipso log={logData} key={index} />
+                    ))}
+                  </div>
+                )}
+              >
+                <div>
+                  <Label
+                    min
+                    color="dark"
+                    text={`${viewLog.data.length}`}
+                  />
+                </div>
+              </Tipso>
             </div>
             <div className={styles.viewLogBottom}>
-              <div className={styles.viewLogAddr}>
-                {viewLog.ipInfo.addr}
+              <div className={styles.viewRow}>
+                {viewLog.addr}
               </div>
-              {minutesBefore(viewLog.updatedAt)}
+              {minutesBefore(viewLog.datetime)}
             </div>
           </div>
         ))}
