0b3f3898f2098432c362ec0abcc1e53f64725dda
ecmadao <wlec@outlook.com>
2020-08-18 23:05:52 +0800

    Fix typo

--- frontend/utils/locales/resume/en.js
+++ frontend/utils/locales/resume/en.js
@@ -69,7 +69,7 @@ const datas = {
       graduateAt: 'Graduate at',
       addEduExperience: 'What did you do in your college?',
       introList: [],
-      introText: 'Write the activities you joined or achievements, then press enter to create it'
+      introText: 'Write the activities you joined or achievements, then press enter to create'
     },
     workExperiences: {
       title: 'Work experience',
@@ -89,7 +89,7 @@ const datas = {
       projectHomepage: 'Project homepage',
       addProjectDesc: 'Add project desc',
       introList: [],
-      introText: 'Wirte the project desc then press enter to create it',
+      introText: 'Wirte the project desc then press enter to create',
       techIntroText: 'Press Enter to add',
     },
     personalProjects: {
@@ -110,11 +110,11 @@ const datas = {
       yourDream: 'Your dream/slogan?',
       personalIntro: 'Personal intro/Self assessment',
       introList: [],
-      introText: 'Wirte your personal intro then press enter to create it',
-      selfAssessment: 'Self Assessment',
+      introText: 'Wirte your personal intro then press enter to create',
+      selfAssessment: 'Self assessment',
       and: ' and ',
       links: {
-        title: 'Other Links',
+        title: 'Other links',
         github: 'Your GitHub link',
         segmentfault: 'Your Segmentfault link',
         blog: 'Your blog link',
@@ -131,7 +131,7 @@ const datas = {
       homepage: 'Related url',
       introText: 'Description for your section',
       sectionTitle: 'title',
-      addSectionDetail: 'Add description, then press Enter to add it'
+      addSectionDetail: 'Write description, then press Enter to add'
     }
   },
   navs: {
